769758cd43c1fa326b39b51a16378d7641949dcc
Alexandr Derevyanko <7171sites@gmail.com>
2023-01-16 12:11:49 +0800

    first commit

---
+++ MyHabits/MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Aleksandr Derevyanko on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+    }
+
+
+}
+
